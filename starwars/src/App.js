f28c519cb2083f779cb053b05e09f311b57c66f7
Nate Magera <nmagera07@gmail.com>
2019-05-31 13:03:37 -0400

    added further styling to the page

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -36,9 +36,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <p>{this.state.starwarsChars.map(char => (
-            <Characters key={char.url} char={char} /> 
-            ))}</p>
+        <h1 className="Header">Star Wars Character's Info</h1>
+          <div className="list">
+            <p className="app-p">{this.state.starwarsChars.map(char => (
+                <Characters key={char.url} char={char} /> 
+                ))}</p>
+            </div>
       </div>
     );
   }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './StarWars.css'
+import '../App.css'
 
 class Characters extends React.Component {
     render() {
         return (
-            <div class="chars">
-                <div class="chars-1">
+            <div className="chars">
+                
                 <p>Name: {this.props.char.name}</p>
                 <p>Birth Year: {this.props.char.birth_year}</p>
                 <p>Gender: {this.props.char.gender}</p>
                 <p>Eye Color: {this.props.char.eye_color}</p>
-                </div>
+                
             </div>
         )
     }
